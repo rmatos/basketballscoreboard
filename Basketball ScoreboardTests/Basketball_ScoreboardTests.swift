d8a5d6c595f8e48a5ba368f9da46033f3c843ab6
Rudy E. Matos Perez <rudymatos@Rudys-MacBook-Pro.local>
2015-09-05 12:54:29 -0400

    Initial Commit

---
+++ Basketball ScoreboardTests/Basketball_ScoreboardTests.swift
@@ -0,0 +1,36 @@
+//
+//  Basketball_ScoreboardTests.swift
+//  Basketball ScoreboardTests
+//
+//  Created by Rudy E. Matos Perez on 9/5/15.
+//  Copyright (c) 2015 Rudy Matos. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Basketball_ScoreboardTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
